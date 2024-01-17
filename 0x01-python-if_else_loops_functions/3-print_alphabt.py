fc32de7606596e8a5dc180a9877732d91cd45c55
MANHYIA <gmanhyia@gmail.com>
2024-01-17 14:57:55 -0800

    all

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for number in range(97,122):
+for number in range(97,123):
     if number !=101 and number !=113:   
         print('{:c}'.format(number), end="")
     number= number+1
